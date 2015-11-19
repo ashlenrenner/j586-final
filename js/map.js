d7f90d1e4f50917f45a21c7b0831a0172b97a191
ashlen-renner <ashlenrenner@gmail.com>
2015-11-18 20:53:01 -0500

    making markers

--- js/map.js
+++ js/map.js
@@ -1,4 +1,3 @@
-
 function initMap() {
   console.log("map loaded");
   map = new google.maps.Map(document.getElementById('map'), {
@@ -6,4 +5,40 @@ function initMap() {
     zoom: 6,
     mapTypeId: google.maps.MapTypeId.HYBRID
   });
+
+// ajax call for the map
+var park = [];
+var lat = [];
+var long = [];
+var title = [];
+
+$(document).ready(function() {
+
+  $.ajax({
+      type: 'GET',
+      url: 'parks.xml',
+      dataType: 'xml',
+      success: parseData
+    });
+    function parseData(xml){
+
+  $(xml).find("park").each(function(){
+    var $park = $(this);
+    title.push(parseInt($park.find("title").text()));
+    lat.push(parseInt($park.find("lat").text()));
+    long.push(parseInt($park.find("long").text()));
+
+  })
+
+  buildMarker();
+} //closes parseData function
+
+function buildMarker(xml){
+  var marker = new google.maps.Marker({
+    position: {lat, long},
+    map: map,
+    title: title
+  });
+}
+})
 }
