baa799ce141da4e55c63f40efb02baf1ac68763c
ashlen-renner <ashlenrenner@gmail.com>
2015-11-18 20:08:12 -0500

    set map

--- js/map.js
+++ js/map.js
@@ -2,7 +2,8 @@
 function initMap() {
   console.log("map loaded");
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 35.5000, lng: -80.0000},
+    zoom: 6,
+    mapTypeId: google.maps.MapTypeId.HYBRID
   });
 }
